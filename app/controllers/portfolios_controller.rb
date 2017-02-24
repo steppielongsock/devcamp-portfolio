7e099390e6a5e065db615205df090cdbae2741c4
Stephanie Pinion <stephpinion@gmail.com>
2017-02-24 15:02:48 -0700

    Added Delete Record to Portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -40,7 +40,18 @@ class PortfoliosController < ApplicationController
 
 def show
   @portfolio_item = Portfolio.find(params[:id])
-
 end
+
+  def destroy
+    # perform the lookup
+    @portfolio_item = Portfolio.find(params[:id])
+
+    # Distroy the record
+    @portfolio_item.destroy
+
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
+    end
+  end
 
 end
