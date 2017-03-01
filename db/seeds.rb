10e2fa0481bada96af4e1701320edf162093b161
Stephanie Pinion <stephpinion@gmail.com>
2017-03-01 00:01:19 -0700

    Integrated Technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -48,7 +48,16 @@ puts "5 skills posts created"
     body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
     main_image: "http://placehold.it/600x400",
     thumb_image: "http://placehold.it/350x200"
-    )
+  )
   end
 
   puts "8 Portfolios posts created"
+
+  3.times do |technology|
+    Portfolio.last.technologies.create!(
+      name: "Technology #{technology}"
+      
+    )
+  end
+
+   puts "3 Technologies created"
