f0eadd8c3348d98246d67f8fad29679b8a11558c
Stephanie Pinion <stephpinion@gmail.com>
2017-03-02 13:30:48 -0700

    installed devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :portfolios, except: [:show]
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
-
-
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
