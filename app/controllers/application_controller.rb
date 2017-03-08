f3ba09a06f219f1fff5d466cfbf2bc4a10ce080e
Stephanie Pinion <stephpinion@gmail.com>
2017-03-08 13:51:20 -0700

    Updated copyright module to Pinionviewtool, added gem to gem file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,16 +8,6 @@ class ApplicationController < ActionController::Base
   before_action :set_copyright
 
   def set_copyright
-    @copyright = DevcampViewTool::Renderer.copyright 'Stephanie Pinion', 'All rights reserved'
+    @copyright = PinionViewTool::Renderer.copyright 'Stephanie Pinion', 'All rights reserved'
   end
-
-end
-
-module DevcampViewTool
-  class Renderer
-    def self.copyright name, msg
-      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
-    end
-  end
-
 end
