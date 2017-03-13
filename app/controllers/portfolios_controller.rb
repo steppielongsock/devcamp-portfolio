84b24de2b1f84a92c4c45025e0d74cdbd3da88f6
Stephanie Pinion <stephpinion@gmail.com>
2017-03-13 11:10:34 -0600

    Updated portfolio to include attr for position

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -5,7 +5,7 @@ class PortfoliosController < ApplicationController
   before_action :set_portfolio, only:[:edit, :update, :show, :destroy]
 
   def index
-    @portfolio_items = Portfolio.all
+    @portfolio_items = Portfolio.by_position
   end
 
   def angular
